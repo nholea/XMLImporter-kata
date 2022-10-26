ba493d4d72d5644e9c7e2b2cfdc8baa2a0dc78d2
Natalia <natalia.hernandez@clarity.ai>
2022-10-26 12:09:29 +0200

    feat: create SalaryWarehouse class and move methods related to salary into it

---
+++ XMLImporter/src/main/java/SalaryWarehouse.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import xmlmodels.Staff;
+
+public class SalaryWarehouse {
+
+  private void insertSalary(Connection connection, Staff staff) throws SQLException {
+    try (PreparedStatement preparedStatement = connection.prepareStatement(
+      "INSERT INTO salary(staff_id, currency, value) VALUES (?,?,?)")) {
+      preparedStatement.setInt(1, staff.id);
+      preparedStatement.setString(2, staff.salary.currency);
+      preparedStatement.setInt(3, staff.salary.value);
+      preparedStatement.executeUpdate();
+    }
+  }
+
+}
