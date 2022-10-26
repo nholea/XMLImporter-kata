1561c751028ca0ab90e6fc84e4e2e9eedbe8ef7e
Natalia <natalia.hernandez@clarity.ai>
2022-10-26 12:06:13 +0200

    feat: create CompanyWarehouse class and move methods related to company into it

---
+++ XMLImporter/src/main/java/CompanyWarehouse.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import xmlmodels.Company;
+
+public class CompanyWarehouse {
+
+  public int insertCompanyNameAndGetGeneratedKeyAnd(Company company, Connection connection) throws SQLException {
+    try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO company(name) VALUES (?)",
+      Statement.RETURN_GENERATED_KEYS)) {
+      preparedStatement.setString(1, company.name);
+      preparedStatement.executeUpdate();
+
+      return getCompanyId(preparedStatement);
+    }
+  }
+
+  private int getCompanyId(PreparedStatement preparedStatement) throws SQLException {
+    int companyId;
+    try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+      if (generatedKeys.next()) {
+        companyId = (int) generatedKeys.getLong(1);
+      } else {
+        throw new SQLException("No ID obtained.");
+      }
+    }
+    return companyId;
+  }
+
+}
